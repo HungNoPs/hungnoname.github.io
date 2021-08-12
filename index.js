f06ae18583cace228e0a08b120cbdd0923d2214d
HungYLL <nguyenhung.it.yll@gmail.com>
2021-08-12 23:05:45 +0700

    load

---
+++ index.js
@@ -0,0 +1,14 @@
+const swiper = new Swiper(".banner", {
+  loop: true,
+  effect: "fade",
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+
+
